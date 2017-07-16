5c47e44e4d5e6c02216715f82f38d7dca701d791
SummerTech <SummerTech@10.144.4.17>
2017-07-16 17:11:32 -0400

    Review

---
+++ Eddie 1/src/Summertech_1.java
@@ -0,0 +1,63 @@
+
+public class Summertech_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//boolean x = (5== 7); 
+		//x=true;
+		
+	/*	if (x )
+		{
+			System.out.println ("x is true");
+		}
+		else
+		{
+			System.out.println (" x is false");
+		}
+		
+		for(int i=0;i<10;i++)
+		{
+			System.out.println(i + " Hello");
+		}	
+		
+		int y=0;
+		while (y<10)
+		{
+			System.out.println(y + " Bye");
+			y++;
+		}
+		
+		for (float z=0;z<1;z=z+.1f)
+		{
+			System.out.println(z);
+		}
+	*/
+	
+	/*for(int y=1;y<6;y++)
+	{
+		for (int x=1;x<6;x++)
+		{
+			System.out.print ("x");
+		}
+		System.out.print("\n");
+	
+	}
+	*/
+	
+		for (int x=1;x<6;x++)
+		{
+			
+			for (int y=0;y<x;y++)
+			{
+				System.out.print("x ");
+			}
+			System.out.println();
+		}
+		
+	
+
+	}
+	
+
+}
