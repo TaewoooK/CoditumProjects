824bfabaeb9e85b26c57268e23d287f492ddd7f8
coditum <coditum@10.144.4.8>
2017-11-06 17:11:18 -0500

    Finish Binary Search and barely started insertion

--- Eddie 1/src/Binary_Search.java
+++ Eddie 1/src/Binary_Search.java
@@ -7,47 +7,66 @@ public class Binary_Search {
 		Scanner input=new Scanner(System.in);
 		System.out.println("Which number in the array do you want to fine the binary number for?");
 
-		int []binArray=new int [7];
-
+		int []binArray={1, 5, 6, 8, 10,};
 		System.out.print("The Array: ");
-		for (int i=0;i <7; i++)
+
+		for (int i=0;i<binArray.length;i++)
 		{
-			binArray[i]=i+1;
 			System.out.print( binArray[i] + " ");	
 		}
 
 		System.out.println("");
 		int user=input.nextInt();
+		
 		int mid=0;
 		int two =2;
 		int end;
 		int start;
 		int sub=0;
 
-		end = binArray.length-1;
+		end = binArray.length;
 		start =0;
+
+		sub= end-start;
+		mid=sub/two;
 
 		boolean found = false;
 		while (!(found))
 		{
-			sub= end-start;
-			mid=sub/two;
-
-			if (binArray[mid]==user)
+//			System.out.println(start);
+//			System.out.println(end);
+			if (end-start == 1)
 			{
-			System.out.println("Found");	
+			System.out.println("The number is not in the array");
 			break;
 			}
-			else if (binArray[mid]>user)
-			{
-				end=mid;
+			
+
+			if (binArray[mid]==user)
+			{	
+				System.out.println("The target of the number is "+ mid);	
+
+				break;
 			}
-			else
+			else if (user>binArray[mid])
 			{
 				start=mid;
+				sub= end-start;
+				mid=(sub/two)+start;
 			}
-			System.out.println(mid);
+
+			else if(user<binArray[mid])
+			{
+				end=mid;
+				sub= end-start;
+				mid=sub/two;
+			}
+			else{
+				break;
+			}
+
 		}
 
 	}
+
 }
---
+++ Eddie 1/src/Insertion_Sort.java
@@ -0,0 +1,22 @@
+
+public class Insertion_Sort {
+
+	public static void main(String[] args) {
+
+		int []insertion={6,8,3,10,2,55};
+		
+		for (int i=0;i<insertion.length;i++)
+		{
+			sort(insertion[i]);
+		}
+		
+
+	}
+	
+	public static void sort(int array)
+	{
+		System.out.print(array + " ");
+		
+	}
+
+}
