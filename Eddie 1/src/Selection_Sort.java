676eaed4f85f4929dc25b7fd7125c7d1f4609f0f
coditum <coditum@10.144.4.12>
2017-12-04 17:08:32 -0500

    Finished Selection Sort

--- Eddie 1/src/Selection_Sort.java
+++ Eddie 1/src/Selection_Sort.java
@@ -3,25 +3,45 @@ public class Selection_Sort {
 
 	public static void main(String[] args) {
 
-		int []SelArray= {33,2,52,106,73};
-		int small=SelArray[0];
+		int []SelArray= {33,2,52,108,73};
 
 		for (int i=0;i<SelArray.length;i++)
 		{
 			System.out.print(SelArray[i]+ " ");
 		}
 		System.out.println();
-		
-		for(int n=1; n<SelArray.length;n++)
+		System.out.println("After Selection Sorting...");
+		int indx =0;
+
+		for (int x=1;x<SelArray.length;x++)
 		{
-			if (SelArray[n]<small)
+			int small=SelArray[x-1];
+			boolean newsmall = false;
+
+			for(int n=x; n<SelArray.length;n++)
 			{
-				small= SelArray[n];
-				System.out.println (small);
+				if (SelArray[n]<small)
+				{
+					small = SelArray[n];
+					indx = n;
+					newsmall = true;
+				}
 			}
 
+			if(newsmall)
+			{
+				int temp = SelArray[x-1];
+				SelArray[x-1]=small;
+				SelArray[indx]=temp;
+				//	System.out.println(small);
+			}
 		}
 
-	}
+		for (int i=0;i<SelArray.length;i++)
+		{
+			System.out.print(SelArray[i]+ " ");
+		}
+		System.out.println();
 
+	}
 }
