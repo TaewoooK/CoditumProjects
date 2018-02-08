3b2c5943f42d8bee22253fd33c14ddda8045c4aa
coditum <coditum@10.144.4.20>
2018-02-08 16:59:23 -0500

    Finished Merge Sort Next, create new class and start quick sort

--- Eddie 1/src/Merge_Sort.java
+++ Eddie 1/src/Merge_Sort.java
@@ -1,76 +1,87 @@
-
 public class Merge_Sort {
 
 	public static void main(String[] args) {
 
-		int []arr = {33,2,52,108,73,22,12,78};
-
+		int []arr = {2, 4, 1, 3, 6, 5, 7, 8};
 
 		System.out.println("Before:");
+
 		for (int i=0; i<arr.length;i++)
 		{
 			System.out.print(arr[i] + " ");
 		}
 		System.out.println();
-		
 		mergesort(arr, 0, arr.length-1);
 
-		System.out.println(merge(arr, 0, (arr.length)/2,arr.length-1));
+		for (int i=0; i<arr.length; i++)
+		{	
+		System.out.print(arr[i] + " ");
+		}
 
 	}
-
 
 	public static void mergesort(int []arr, int left, int right )
 	{
-		int mid = (left + right)/2;
-		
-		if (right-left!=0)
+		if (left<right)
 		{
+			int mid = (left + right)/2;
 			mergesort(arr, left, mid);
 			mergesort (arr, mid+1, right);
 			merge(arr, left, mid, right);
-					
 		}	
-		
+
 	}
 
-	public static int merge(int []arr, int left, int mid, int right)
+	public static void merge(int []arr, int left, int mid, int right)
 	{
 		int lsize= mid-left+1;
 		int rsize= right-mid;
 		
 		int L[]=new int[lsize];
 		int R[]=new int[rsize];
-		
-		for (int i=0;i<lsize;i++)
+
+		for (int i=0;i<lsize;++i)
 		{
 			L[i]=arr[left+i];
 		}
-		for (int j=0;j<rsize;j++)
+		for (int j=0;j<rsize;++j)
 		{
 			R[j]= arr[mid+1+j];
 		}
+
+		int i=0;
+		int j=0;
+		int x=left;
 		
-		
-		
-		
-		if (mid == (left+right)/2)
+		while (i<lsize && j<rsize)
 		{
-			return merge (arr, left, mid, right);
-		}
-		else 
-		{
-			if (mid>left)
+			if (L[i]<= R[j])
 			{
-				merge(arr, left, mid, right );
+				arr[x]=L[i];
+				i++;
 			}
-			else if(left>mid)
+			else 
 			{
-				merge (arr, mid, left, right);
-			}
-			return 0;
+				arr[x]=R[j];
+				j++;
+			}	
+			x++;
 		}
 
+		while (i<lsize)
+		{
+			arr[x]=L[i];
+			i++;
+			x++;
+		}
+		
+		while (j<rsize)
+		{
+			arr[x]=R[j];
+			j++;
+			x++;
+		}
+		
 	}
 
 }
