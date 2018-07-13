73e6e68cfa9eac91b3fff1e501528f104620c86a
eddie <eddie@192.168.3.110>
2018-07-13 15:36:11 -0400

    Created new package for summertech

---
+++ Eddie 1/src/summertech_2018/Hello_World.java
@@ -0,0 +1,19 @@
+package summertech_2018;
+
+public class Hello_World {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!");
+		
+		//Quiz Game
+		System.out.println();
+		
+		System.out.println("Welcome to the quiz
+		
+		
+		
+		
+	}
+
+}
